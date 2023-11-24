b10afd7941dccc4c2b4dc228a7198432f8307045
Sumit Paul <pauls1317@gmail.com>
2023-11-24 16:51:58 +0530

    signin and signup components

--- 02-mfp/auth/src/App.js
+++ 02-mfp/auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'auth',
 });
@@ -11,7 +14,10 @@ export default ({ history }) => {
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
-          <Switch></Switch>
+          <Switch>
+            <Route path="/auth/signin" component={Signin} />
+            <Route path="/auth/signup" component={Signup} />
+          </Switch>
         </Router>
       </StylesProvider>
     </div>
